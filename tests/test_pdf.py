dad7db524c730bc2f97304b5a52d4b762b99f87f
Vitalii Honchar <weaxme@gmail.com>
2024-02-16 07:58:30 -0500

    Added tests

---
+++ tests/test_pdf.py
@@ -0,0 +1,24 @@
+from article_improver import pdf
+from uuid import uuid4
+import pytest
+import fitz
+
+
+def test_return_text_if_file_reading_was_successful():
+    # GIVEN
+    filename = "tests/test.pdf"
+    expected = "Test content"
+
+    # WHEN
+    actual = pdf.read_pdf(filename)    
+
+    # THEN
+    assert actual == expected
+
+def test_raise_exception_if_file_not_exists():
+    # GIVEN
+    filename = str(uuid4())
+
+    # WHEN-THEN
+    with pytest.raises(fitz.FileNotFoundError):
+        pdf.read_pdf(filename)    
