a80b458810ed5251b7bc24ead70b7c6667a2da7b
Vitalii Honchar <weaxme@gmail.com>
2024-02-16 07:38:28 -0500

    Added pdf file

---
+++ src/article_improver/pdf.py
@@ -0,0 +1,9 @@
+import fitz
+
+
+def read_pdf(filename: str) -> str:
+    with fitz.open(filename) as doc:
+        text = ""
+        for page in doc:
+            text += page.get_text()
+        return text
