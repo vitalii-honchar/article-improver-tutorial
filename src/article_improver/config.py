da323d0b3c4b34240ffff8b4052f165522cd599b
Vitalii Honchar <weaxme@gmail.com>
2024-02-16 08:29:44 -0500

    Added config.py

---
+++ src/article_improver/config.py
@@ -0,0 +1,39 @@
+import asyncio
+import sys
+import yaml
+from dataclasses import dataclass
+from loguru import logger
+import os
+
+DEFAULT_CONFIG_FILE_FOLDER = f"{os.path.expanduser('~')}/.config/articleimprover"
+DEFAULT_CONFIG_FILE = "config.yaml"
+
+FIELD_OPEN_AI_KEY = "open_ai_key"
+
+
+@dataclass
+class Config:
+    open_ai_key: str
+
+
+def _read_config(config_file_folder: str, config_file: str) -> Config:
+
+    if config_file_folder is None or config_file is None:
+        file = f"{DEFAULT_CONFIG_FILE_FOLDER}/{DEFAULT_CONFIG_FILE}"
+    else:
+        file = f"{config_file_folder}/{config_file}"
+
+    with open(file) as f:
+        config_json = yaml.safe_load(f)
+        return Config(config_json[FIELD_OPEN_AI_KEY])
+
+
+def init(config_file_folder: str=None, config_file: str=None) -> tuple[Config, asyncio.AbstractEventLoop]:
+    logger.remove()
+    logger.add(sys.stderr, format="{message}", level="INFO")
+    try:
+        cfg = _read_config(config_file_folder, config_file)
+    except:
+        print("No valid configuration file!")
+        cfg = None
+    return (cfg, asyncio.get_event_loop_policy().get_event_loop())
---
+++ tests/test_config.py
@@ -0,0 +1,44 @@
+from article_improver import config
+from uuid import uuid4
+import os
+
+
+def test_return_config_if_file_exists():
+    # GIVEN
+    folder = "tests"
+    filename = "test_config.yaml"
+    expected_open_ai_key = "test-key"
+
+    # WHEN
+    cfg, loop = config.init(folder, filename)
+
+    # THEN
+    assert loop is not None
+    assert cfg is not None
+    assert cfg.open_ai_key == expected_open_ai_key
+
+def test_return_none_if_file_not_exists():
+    # GIVEN
+    folder = str(uuid4())
+    filename = f"{str(uuid4())}.yaml"
+
+    # WHEN
+    cfg, loop = config.init(folder, filename)
+
+    # THEN
+    assert loop is not None
+    assert cfg is None
+
+def test_default_config_file_folder():
+    # GIVEN
+    expected = f"{os.path.expanduser('~')}/.config/articleimprover"
+
+    # WHEN-THEN
+    assert config.DEFAULT_CONFIG_FILE_FOLDER == expected
+
+def test_default_config_file():
+    # GIVEN
+    expected = "config.yaml"
+
+    # WHEN-THEN
+    assert config.DEFAULT_CONFIG_FILE == expected
