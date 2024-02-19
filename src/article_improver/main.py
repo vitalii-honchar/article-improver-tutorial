d8b4060ee185575c502cb02b1b527f25bfbcc30e
Vitalii Honchar <weaxme@gmail.com>
2024-02-19 08:20:04 -0500

    Added calling config command

--- src/article_improver/main.py
+++ src/article_improver/main.py
@@ -1,10 +1,17 @@
 import typer
+from article_improver import config
+from article_improver.command import config_command
 
+cfg, loop = config.init()
 app = typer.Typer()
 
 @app.command(name="seo")
 def seo_command():
     print("Seo command")
+
+@app.command(name="configure")
+def configure_command():
+    config_command.handle(config.DEFAULT_CONFIG_FILE, config.DEFAULT_CONFIG_FILE_FOLDER)
 
 if __name__ == "__main__":
     app()
